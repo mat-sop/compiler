dbe4dc96990eaf4d858b6abf244276abf0076b44
Mateusz Sopiński <mateuszsopinski243@gmail.com>
2019-12-27 20:10:27 +0100

    Add lexer rules

---
+++ compiler/lexer.py
@@ -0,0 +1,86 @@
+import re
+
+import ply.lex as lex
+
+
+tokens = (
+    'COMMENT', 'SEMICOLON', 'COLON', 'LEFTB', 'RIGHTB',
+
+    'PLUS', 'MINUS', 'TIMES', 'DIV', 'MOD',
+    'EQ', 'NEQ', 'LE', 'GE', 'LEQ', 'GEQ',
+    'ASSIGN',
+    'IF', 'THEN', 'ELSE', 'ENDIF',
+    'WHILE', 'DO', 'ENDWHILE', 'ENDDO',
+    'FOR', 'FROM', 'TO', 'DOWNTO', 'ENDFOR',
+    'READ', 'WRITE',
+    'DECLARE', 'BEGIN', 'END',
+    'NUM', 'ID'
+)
+
+t_PLUS = 'PLUS'
+t_MINUS = 'MINUS'
+t_TIMES = 'TIMES'
+t_DIV = 'DIV'
+t_MOD = 'MOD'
+
+t_EQ = 'EQ'
+t_NEQ = 'NEQ'
+t_LE = 'LE'
+t_GE = 'GE'
+t_LEQ = 'LEQ'
+t_GEQ = 'GEQ'
+
+t_ASSIGN = 'ASSIGN'
+
+t_IF =  'IF'
+t_ELSE = 'ELSE'
+t_THEN = 'THEN'
+t_ENDIF = 'ENDIF'
+
+t_WHILE = 'WHILE'
+t_DO = 'DO'
+t_ENDWHILE = 'ENDWHILE'
+t_ENDDO = 'ENDDO'
+
+t_FOR = 'FOR'
+t_FROM = 'FROM'
+t_TO = 'TO'
+t_DOWNTO = 'DOWNTO'
+t_ENDFOR = 'ENDFOR'
+
+t_READ = 'READ'
+t_WRITE = 'WRITE'
+
+t_DECLARE = 'DECLARE'
+t_BEGIN = 'BEGIN'
+t_END = 'END'
+
+t_NUM = '[0-9]+'
+t_ID = '[_a-z]+'
+
+t_COLON = ','
+t_SEMICOLON = ';'
+t_LEFTB = '\('
+t_RIGHTB = '\)'
+
+t_ignore = ' \t'
+
+
+def t_COMMENT(t):
+    '\[.*?\]'
+    pass
+
+
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += t.value.count('\n')
+    print(t.lexer.lineno)
+
+
+def t_error(t):
+    print("Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)
+
+
+lexer = lex.lex()
+lex.runmain()
