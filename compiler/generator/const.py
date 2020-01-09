e1cae5a8eeffb188bd4dbdb17a508ae362b11f02
Mateusz Sopiński <mateuszsopinski243@gmail.com>
2020-01-10 00:25:48 +0100

    Change constance generation algorithm

--- compiler/generator/const.py
+++ compiler/generator/const.py
@@ -1,31 +1,9 @@
-import math
-
-LIMIT = 64
-
-
-def gen_const(n, index):   # TODO MAKE FASTER
+def gen_const(n, index):
     sign_c = 'INC' if n > 0 else 'DEC'
-    pows = []
-    while n > LIMIT:
-        p = math.floor(math.log2(n))
-        pows.append(p)
-        n -= 2**p
-
-    commands = ['SUB 0', 'STORE 2', 'STORE 1']
-    last_pow = 0
-    for p in reversed(pows):
-        commands += ['LOAD 1']
-        commands += (p - last_pow) * ['INC']
-        last_pow = p
-        commands += [
-            'STORE 1',
-            'SUB 0',
-            sign_c,
-            'SHIFT 1',
-            'ADD 2',
-            'STORE 2']
-    commands += ['LOAD 2']
-    commands += n * [sign_c]
+    commands = ['SUB 0', 'INC', 'STORE 1', 'SUB 0']
+    for b in bin(n)[2:]:
+        commands += ['SHIFT 1']
+        if b == '1':
+            commands += [sign_c]
     commands += [f'STORE {index}']
-
     return commands
