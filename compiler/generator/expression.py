7c89d808409587e9f8d3bb99e413165d588c84db
Mateusz Sopiński <mateuszsopinski243@gmail.com>
2020-01-29 16:38:21 +0100

    Fix division

--- compiler/generator/expression.py
+++ compiler/generator/expression.py
@@ -128,136 +128,6 @@ def times(index1, index2):
     ]
 
 
-def div(index1, index2):
-    '''
-    def div(index1, index2):
-        a = index1
-        b = index2
-        factor = 1
-        left = a
-        result = 0
-        tmp = b
-        while(factor < left):  # loop1
-            factor *= 2
-            tmp *= 2
-
-        while(left >= b):  # loop2
-            while (tmp > left):  # loop3
-                factor = factor/2
-                tmp = tmp/2
-            result += factor
-            left = left - tmp
-        return result
-    '''
-
-    factor = 2
-    left = 3
-    result = 4
-    tmp = 5
-    pow2 = 6
-    a = 7
-    b = 8
-
-    initialize = [
-        f'LOAD {a}',
-        f'STORE {left}',
-        f'LOAD {b}',
-        f'STORE {tmp}',
-        'SUB 0',
-        f'STORE {result}',
-        f'INC',
-        f'STORE {factor}',
-        f'STORE {pow2}'
-    ]
-
-    loop1_condition = con_le(factor, left)
-    loop1_commands = [
-        f'LOAD {factor}',
-        f'SHIFT {pow2}',
-        f'STORE {factor}',
-        f'LOAD {tmp}',
-        f'SHIFT {pow2}',
-        f'STORE {tmp}',
-    ]
-    loop1 = while_do(loop1_condition, loop1_commands)
-
-    loop3_condition = con_ge(tmp, left)
-    loop3_commands = [
-        f'LOAD {factor}',
-        f'SHIFT {pow2}',
-        f'STORE {factor}',
-        f'LOAD {tmp}',
-        f'SHIFT {pow2}',
-        f'STORE {tmp}',
-    ]
-    loop3 = [
-        'SUB 0',
-        'DEC',
-        f'STORE {pow2}',
-        *while_do(loop3_condition, loop3_commands)
-    ]
-
-    loop2_condition = con_geq(left, b)
-    loop2_commands = [
-        *loop3,
-        f'LOAD {result}',
-        f'ADD {factor}',
-        f'STORE {result}',
-        f'LOAD {left}',
-        f'SUB {tmp}',
-        f'STORE {left}'
-    ]
-    loop2 = while_do(loop2_condition, loop2_commands)
-
-    change_signs_to_plus = [
-        f'LOAD {index1}',
-        'JPOS k_3',
-        f'SUB {index1}',
-        f'SUB {index1}',
-        f'STORE {a}',
-        f'LOAD {index2}',
-        'JPOS k_3',
-        f'SUB {index2}',
-        f'SUB {index2}',
-        f'STORE {b}',
-    ]
-
-    fix_sign = [
-        f'LOAD {index1}',
-        'JPOS k_6',
-        f'LOAD {result}',
-        f'SUB {result}',
-        f'SUB {result}',
-        f'DEC',
-        f'STORE {result}',
-
-        f'LOAD {index2}',
-        'JPOS k_6',
-        f'LOAD {result}',
-        f'SUB {result}',
-        f'SUB {result}',
-        'DEC',
-        f'STORE {result}'
-    ]
-
-    division = [
-        *change_signs_to_plus,
-        *initialize,
-        *loop1,
-        *loop2,
-        *fix_sign,
-        f'LOAD {result}'
-    ]
-
-    return [
-        f'LOAD {index2}',
-        f'JZERO k_{len(division)+3}',
-        f'LOAD {index1}',
-        f'JZERO k_{len(division)+1}',
-        *division
-    ]
-
-
 def mod(index1, index2):
     '''
     def modd(index1, index2):
@@ -396,4 +266,121 @@ def mod(index1, index2):
         f'JZERO k_{len(fix_sign)+2}',
         *fix_sign,
         f'LOAD {remain}'
+    ]
+
+
+def div(index1, index2):
+    scaled_divisor = 2
+    remain = 3
+    result = 4
+    multiple = 5
+    pow2 = 6
+    a = 7
+    b = 8
+
+    initialize = [
+        f'LOAD {b}',
+        f'STORE {scaled_divisor}',
+        f'LOAD {a}',
+        f'STORE {remain}',
+        'SUB 0',
+        f'STORE {result}',
+        'INC',
+        f'STORE {multiple}',
+        f'STORE {pow2}'
+    ]
+
+    loop1_condition = con_le(scaled_divisor, a)
+    loop1_commands = [
+        f'LOAD {scaled_divisor}',
+        f'SHIFT {pow2}',
+        f'STORE {scaled_divisor}',
+        f'LOAD {multiple}',
+        f'SHIFT {pow2}',
+        f'STORE {multiple}'
+    ]
+    loop1 = while_do(loop1_condition, loop1_commands)
+
+    if1_condition = con_geq(remain, scaled_divisor)
+    if1_commands = [
+        f'LOAD {remain}',
+        f'SUB {scaled_divisor}',
+        f'STORE {remain}',
+        f'LOAD {result}',
+        f'ADD {multiple}',
+        f'STORE {result}'
+    ]
+    if1 = if_then(if1_condition, if1_commands)
+
+    loop2_commands = [
+        *if1,
+        f'LOAD {scaled_divisor}',
+        f'SHIFT {pow2}',
+        f'STORE {scaled_divisor}',
+        f'LOAD {multiple}',
+        f'SHIFT {pow2}',
+        f'STORE {multiple}'
+    ]
+    loop2 = [
+        'SUB 0',
+        'DEC',
+        f'STORE {pow2}',
+        *loop2_commands,
+        f'LOAD {multiple}',
+        'JZERO k_2',
+        f'JUMP k_{-len(loop2_commands)-2}'
+    ]
+
+    change_signs_to_plus = [
+        f'LOAD {index1}',
+        'JPOS k_3',
+        f'SUB {index1}',
+        f'SUB {index1}',
+        f'STORE {a}',
+        f'LOAD {index2}',
+        'JPOS k_3',
+        f'SUB {index2}',
+        f'SUB {index2}',
+        f'STORE {b}',
+    ]
+
+    fix_sign = [
+            f'LOAD {index1}',
+            'JPOS k_5',
+            f'LOAD {result}',
+            f'SUB {result}',
+            f'SUB {result}',
+            f'STORE {result}',
+
+            f'LOAD {index2}',
+            'JPOS k_5',
+            f'LOAD {result}',
+            f'SUB {result}',
+            f'SUB {result}',
+            f'STORE {result}',
+
+            f'LOAD {remain}',
+            f'JZERO k_6',
+            f'LOAD {result}',
+            f'JPOS k_4',
+            f'LOAD {result}',
+            'DEC',
+            f'STORE {result}'
+        ]
+
+    division = [
+        *change_signs_to_plus,
+        *initialize,
+        *loop1,
+        *loop2,
+        *fix_sign,
+    ]
+
+    return [
+        f'LOAD {index2}',
+        f'JZERO k_{len(division)+4}',
+        f'LOAD {index1}',
+        f'JZERO k_{len(division)+2}',
+        *division,
+        f'LOAD {result}',
     ]
